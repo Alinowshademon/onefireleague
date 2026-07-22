47e22ee94b90fdf3321d313895f345e948930a6f
The∆ne <anemon29ctg@gmail.com>
2026-07-22 12:46:34 +0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,16 +1,39 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
+
 import {
   onAuthStateChanged,
   signOut
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-onAuthStateChanged(auth, (user) => {
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+onAuthStateChanged(auth, async (user) => {
   
-  if (user) {
-    document.getElementById("welcome").innerHTML =
-      "Welcome<br>" + user.email;
-  } else {
+  if (!user) {
     window.location.href = "index.html";
+    return;
+  }
+  
+  document.getElementById("welcome").innerHTML =
+    "Welcome<br>" + user.email;
+  
+  // Check if user is an admin
+  const adminRef = doc(db, "admins", user.uid);
+  const adminSnap = await getDoc(adminRef);
+  
+  if (adminSnap.exists()) {
+    
+    const adminBtn = document.getElementById("adminBtn");
+    
+    adminBtn.style.display = "block";
+    
+    adminBtn.onclick = () => {
+      window.location.href = "admin.html";
+    };
+    
   }
   
 });
