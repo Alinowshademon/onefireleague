f795c34ed01eb0343cc5f615d3403ef13b56a775
The∆ne <anemon29ctg@gmail.com>
2026-07-26 14:34:38 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import { auth } from "./firebase.js";
+
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    window.location.replace("dashboard.html");
+  }
+});
